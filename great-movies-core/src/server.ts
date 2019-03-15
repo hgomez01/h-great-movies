8e6b0882024d226b78ec9502ceab45fe732d7860
Herberth Gomez <gomezgaray04@gmail.com>
2019-03-14 21:54:47 -0600

    Changing server startup
    - Initiating server once the db connection has been done

--- great-movies-core/src/server.ts
+++ great-movies-core/src/server.ts
@@ -2,10 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
-/* */
-const server = express();
+/* Importing database connection */
 const config = require("./configs/db");
 
+/* Creating server */
+const server = express();
+
+/* setting usage of body parser for better experience working with json */
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,10 +18,10 @@ let port = process.env.PORT || 3000;
 /* Creating database connection */
 mongoose
     .connect(config.database)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
-
-// Starting server
-server.listen(port, function () {
-    console.log('Express server listening on - http://localhost:' + port);
-});
+    .then(() => {
+        // Starting server once the connection with the db has been established
+        server.listen(port, function () {
+            console.log(`Express server listening on - http://localhost:${port}`);
+        });
+    })
+    .catch(err => console.log(`Could not establish connection with the database: ${err}`));
