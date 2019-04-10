5dde2e1075b3feb7c5feb6c2875f3ddd7d05c8f8
Herberth Gomez <gomezgaray04@gmail.com>
2019-04-10 11:38:43 -0600

    Fixing client application
    - Added its own .gitignore file for deploying to heroku
    - Adding new packages
    - Adding server file to deploy it using nodejs
    - Adding reactive module for the client application
    - Fixing form and fields for login and register components

--- great-movies-client/src/app/login/login.component.ts
+++ great-movies-client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  submitted = false;
+  loading = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() { 
+    /* TO DO */
+  }
 }
--- great-movies-client/src/app/register/register.component.ts
+++ great-movies-client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+  loading = false;
+
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() { 
+    /* TO DO */
+  }
 }
